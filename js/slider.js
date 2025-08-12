e1668264c782d93b1de6df836ab6aaca69c6e5f3
CJRSantos <rengifosantoscj@gmail.com>
2025-08-12 15:53:44 -0500

    Archivo Guardado

--- js/slider.js
+++ js/slider.js
@@ -1,36 +1,48 @@
-(function(){
-    
+(function() {
+
     const sliders = [...document.querySelectorAll('.testimony__body')];
     const buttonNext = document.querySelector('#next');
     const buttonBefore = document.querySelector('#before');
-    let value;   
+    let value;
 
-    buttonNext.addEventListener('click', ()=>{
-        changePosition(1);
-    });
+    // Variable para el intervalo automático
+    let sliderInterval;
 
-    buttonBefore.addEventListener('click', ()=>{
-        changePosition(-1);
-    });
-
-    const changePosition = (add)=>{
+    const changePosition = (add) => {
         const currentTestimony = document.querySelector('.testimony__body--show').dataset.id;
         value = Number(currentTestimony);
-        value+= add;
+        value += add;
 
+        sliders[value - add - 1].classList.remove('testimony__body--show');
 
-        sliders[Number(currentTestimony)-1].classList.remove('testimony__body--show');
-        if(value === sliders.length+1 || value === 0){
-            value = value === 0 ? sliders.length  : 1;
+        if (value > sliders.length) {
+            value = 1;
+        } else if (value === 0) {
+            value = sliders.length;
         }
 
-        sliders[value-1].classList.add('testimony__body--show');
+        sliders[value - 1].classList.add('testimony__body--show');
+    };
 
-    }
+    const resetInterval = () => {
+        clearInterval(sliderInterval);
+        sliderInterval = setInterval(() => {
+            changePosition(1);
+        }, 5000);
+    };
 
-    // Función que cambia el slider cada 3 segundos
-    setInterval(() => {
+    buttonNext.addEventListener('click', () => {
         changePosition(1);
-    }, 5000);
+        resetInterval();
+    });
+
+    buttonBefore.addEventListener('click', () => {
+        changePosition(-1);
+        resetInterval();
+    });
+
+    // Iniciar intervalo automático
+    resetInterval();
 
 })();
+
